2eab3ffc72a1befaa27bb6da4b4b71489b1a5c79
Jack de Grave <jack.degrave@outlook.com>
2019-06-01 09:27:22 +0000

    Automatically backed up by Learn

--- lib/my_select.rb
+++ lib/my_select.rb
@@ -1,3 +1,12 @@
 def my_select(collection)
- # your code here!
+ def my_select(array)
+  i = 0 
+  new_array = []
+  while i < array.length
+    if 
+    new_array.push(yield array[i])
+    i += 1
+  end
+  new_array
+end  
 end
